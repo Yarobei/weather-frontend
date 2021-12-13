5c92e2bceba7788fe773a4161339fa532f58c2e7
Dmitry Bogodyazh <dima3911784@hotmail.com>
2021-12-13 13:52:19 +0300

    feat: add ErrorBoundary

---
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import ErrorInfo from "./ErrorInfo";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    debugger;
+    return { hasError: true, error: error };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <ErrorInfo error={this.state.error} />;
+    }
+
+    if (!this.state.hasError) {
+      return this.props.children;
+    }
+
+    return null;
+  }
+}
+
+export default ErrorBoundary;
---
+++ src/components/ErrorBoundary/ErrorInfo.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ErrorInfo = ({ error }) => {
+  // const title =
+  //   error instanceof Error
+  //     ? "Что-то пошло нет так"
+  //     : error.result.status;
+  const description = !(error instanceof Error)
+    ? error.result.value
+    : undefined;
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <p>{description}</p>
+      <button onClick={() => document.location.reload()}>
+        Перезагрузить страницу
+      </button>
+    </div>
+  );
+};
+
+export default ErrorInfo;
