41fb236888a155ade2d9e5c0c5d0f73cf4cab2e2
Dmitry Bogodyazh <dima3911784@hotmail.com>
2021-12-21 15:19:14 +0300

    feat: add status check

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,21 @@
-export const handleErrors = (res) => {
+import { useAuthStore } from "../store/auth.store";
+
+export const handleErrors = async (res) => {
   if (res.ok) {
     return res;
   }
-  return Promise.reject({ status: res.status, statusText: res.statusText });
+  switch (res.status) {
+    case 401:
+      useAuthStore.getState().setIsAuthorized(false);
+      break;
+    case 404:
+    case 429:
+    case 403:
+    case 500: {
+      const parsedRes = await res.json();
+      return { status: res.status, statusText: parsedRes.message };
+    }
+    default:
+      return Promise.reject({ status: res.status, statusText: res.statusText });
+  }
 };
