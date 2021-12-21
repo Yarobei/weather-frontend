f69921feb3deeec6bb3fca281bf7680ba2bfa89b
Dmitry Bogodyazh <dima3911784@hotmail.com>
2021-12-21 16:09:49 +0300

    refactor: change add logic, change pin logic, add remove logic

--- src/components/WeatherBlock/WeatherBlockWrap.jsx
+++ src/components/WeatherBlock/WeatherBlockWrap.jsx
@@ -1,78 +1,126 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { Button, Layout, Row, Col } from "antd";
 
-import CityBlock from "../CityBlock/CityBlock";
-import Modal from "../Modal/Modal";
+import { CityBlock } from "../CityBlock/CityBlock";
+import { AddCityModal } from "../AddCityModal/AddCityModal";
+import { Logo } from "../Logo/Logo";
+
+import { logOutRequest } from "../../service/auth/auth.service";
 
 import style from "./weather-block-wrap.module.scss";
 
-const defaultArr = [
-  { index: 0, city: "" },
-  { index: 1, city: "" },
-  { index: 2, city: "" },
-  { index: 3, city: "" },
-  { index: 4, city: "" },
-  { index: 5, city: "" },
-  { index: 6, city: "" },
-  { index: 7, city: "" },
+const { Header, Content } = Layout;
+
+const defaultArray = [
+  { city: "" },
+  { city: "" },
+  { city: "" },
+  { city: "" },
+  { city: "" },
+  { city: "" },
+  { city: "" },
+  { city: "" },
 ];
 
-const WeatherBlockWrap = ({ isAuthorized, setIsAuthorized }) => {
-  const [cities, setCities] = useState(defaultArr);
-  const [modalProps, setModalProps] = useState({ isOpen: false, index: null });
+export const WeatherBlockWrap = () => {
+  const [cities, setCities] = useState(defaultArray);
+  const [modalState, setModalState] = useState({
+    isOpen: false,
+    currentAddCardIndex: null,
+  });
+
+  const logOut = useCallback(() => logOutRequest(), []);
 
   useEffect(() => {
     const storedArrJson = localStorage.getItem("storedCities");
-    try {
-      if (!storedArrJson) {
-        localStorage.setItem("storedCities", JSON.stringify(defaultArr));
-      } else {
-        setCities(JSON.parse(storedArrJson));
-      }
-    } catch (err) {
-      console.log(err);
+    if (!storedArrJson) {
+      localStorage.setItem("storedCities", JSON.stringify({}));
+      return;
     }
+    const storedArray = JSON.parse(storedArrJson);
+    const newArray = defaultArray.map((item, index) => {
+      if (storedArray[index]) {
+        return { ...item, city: storedArray[index], isPinned: true };
+      }
+      return item;
+    });
+    setCities(newArray);
   }, []);
 
-  const handlePinCity = (isPin, index, city) => {
-    const storedArrJson = localStorage.getItem("storedCities");
-    try {
-      const storedArr = JSON.parse(storedArrJson);
-      storedArr.splice(index, 1, {
-        index,
-        city: isPin ? city : "",
-      });
-      localStorage.setItem("storedCities", JSON.stringify(storedArr));
-    } catch (err) {
-      console.log(err);
+  const handlePinCity = (item, index, isPinned) => {
+    const storedCitiesObject = JSON.parse(localStorage.getItem("storedCities"));
+    if (isPinned) {
+      storedCitiesObject[index] = item.city;
+    } else {
+      delete storedCitiesObject[index];
     }
+    localStorage.setItem("storedCities", JSON.stringify(storedCitiesObject));
+  };
+
+  const handleAddCityModalOpen = (index) => {
+    setModalState({
+      isOpen: true,
+      currentAddCardIndex: index,
+    });
+  };
+
+  const handleAddCityModalClose = () => {
+    setModalState({
+      isOpen: false,
+      currentAddCardIndex: null,
+    });
+  };
+
+  const addCity = (city) => {
+    const newCities = [...cities];
+    newCities.splice(modalState.currentAddCardIndex, 1, {
+      city,
+    });
+    setCities(newCities);
+    handleAddCityModalClose();
+  };
+
+  const handleRemoveCity = (index) => {
+    const newCities = [...cities];
+    newCities.splice(index, 1, {
+      city: "",
+    });
+    handlePinCity(undefined, index, false);
+    setCities(newCities);
   };
 
   return (
-    <>
-      <div className={style.wrap}>
-        {cities.map((item) => {
-          return (
-            <CityBlock
-              key={item.index}
-              cityItem={item}
-              isAuthorized={isAuthorized}
-              setIsAuthorized={setIsAuthorized}
-              setModalProps={setModalProps}
-              cities={cities}
-              setCities={setCities}
-              handlePinCity={handlePinCity}
-            />
-          );
-        })}
-      </div>
-      <Modal
-        modalProps={modalProps}
-        setModalProps={setModalProps}
-        cities={cities}
-        setCities={setCities}
+    <Layout className={style.layout}>
+      <Header className={style.header}>
+        <div className={style.headerContent}>
+          <Logo />
+          <Button onClick={logOut}>Logout</Button>
+        </div>
+      </Header>
+      <Content className={style.content}>
+        <Row gutter={16}>
+          {cities.map((item, index) => {
+            return (
+              <Col className={style.col} key={index} span={6}>
+                <CityBlock
+                  cityName={item.city}
+                  handleAddCityModalOpen={() => handleAddCityModalOpen(index)}
+                  handleRemoveCity={() => handleRemoveCity(index)}
+                  isPinned={item.isPinned}
+                  handlePinCity={(isPinned) =>
+                    handlePinCity(item, index, isPinned)
+                  }
+                />
+              </Col>
+            );
+          })}
+        </Row>
+      </Content>
+      <AddCityModal
+        handleAddCity={addCity}
+        handleClose={handleAddCityModalClose}
+        isOpen={modalState.isOpen}
       />
-    </>
+    </Layout>
   );
 };
-
-export default WeatherBlockWrap;
