79b82f2bf7b4e2144b6a1a28e18b89a9bc5efb3c
Dmitry Bogodyazh <dima3911784@hotmail.com>
2021-12-21 16:10:59 +0300

    refactor: change authorization to use login ant password

--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -1,34 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Input, Button, message, Space } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import shallow from "zustand/shallow";
+
+import { Logo } from "../Logo/Logo";
 
 import { login } from "../../service/auth/auth.service";
 
-import style from "./authorization.module.scss";
-import commonStyles from "../../common.module.scss";
+import { useAuthStore } from "../../store/auth.store";
 
-const Authorization = ({ isAuthorized, setIsAuthorized }) => {
-  const authorize = async () => {
-    try {
-      await login();
+import style from "./authorization.module.scss";
+
+export const Authorization = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { setIsAuthorized } = useAuthStore(
+    (state) => ({
+      isAuthorized: state.isAuthorized,
+      setIsAuthorized: state.setIsAuthorized,
+    }),
+    shallow
+  );
+
+  const authorize = async (values) => {
+    setIsLoading(true);
+    const response = await login(values);
+    if (response.ok) {
       setIsAuthorized(true);
-    } catch (err) {
-      setIsAuthorized(false);
+    } else {
+      message.error(response.statusText);
     }
+    setIsLoading(false);
   };
 
   return (
-    <div className={style.wrap}>
-      {isAuthorized ? (
-        <span>You are authorized</span>
-      ) : (
-        <button
-          className={`${style.loginButton} ${commonStyles.animateButton}`}
-          onClick={authorize}
-        >
-          Login
-        </button>
-      )}
-    </div>
+    <Form className={style.wrap} onFinish={authorize}>
+      <Space direction={"vertical"} size={"large"} align={"center"}>
+        <Logo />
+        <Space direction={"vertical"} align={"center"}>
+          <Form.Item
+            name={"username"}
+            rules={[{ required: true, message: "Username is required" }]}
+          >
+            <Input placeholder={"Username"} prefix={<UserOutlined />} />
+          </Form.Item>
+          <Form.Item
+            name={"password"}
+            rules={[{ required: true, message: "Password is required" }]}
+          >
+            <Input
+              type={"password"}
+              placeholder={"Password"}
+              prefix={<LockOutlined />}
+            />
+          </Form.Item>
+          <Button htmlType={"submit"} type={"primary"} loading={isLoading}>
+            Login
+          </Button>
+        </Space>
+      </Space>
+    </Form>
   );
 };
-
-export default Authorization;
